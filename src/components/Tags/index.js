7366fadffb4f505182d4e9c9d917144fb9ade30c
TianYe <mjwdbb@sina.com>
2019-12-25 08:02:01 +0800

    init project

---
+++ src/components/Tags/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class TagsComp extends React.Component {
+	
+	render(){
+		return (<div>这是一个标签！</div>);
+	}
+};
+
+export default TagsComp;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export {default as TagsComp} from './Tags';
